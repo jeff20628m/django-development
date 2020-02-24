8c4564333c5a0c50e37e8b0da12b3c17acad0120
jeffLai <jeff20628m@gmail.com>
2020-02-24 16:14:17 +0800

    'add:allowhost'

--- Django-development/learning_users/learning_users/settings.py
+++ Django-development/learning_users/learning_users/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j&wblwbdq(i*x+r&l0y96-wv%c5bjopqbgix(0s*i#o32%5r*b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','laiyi.pythonanywhere.com']
 
 
 # Application definition
